23de7ba6ffad04ce9d5f8bd9c6f5fd83cb2a9bd8
Luis Morales Bernal <luismcodes@gmail.com>
2022-11-21 21:45:35 -0600

    Initialize Sequelize

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
+const db = require("./app/models");
+const Role = db.role;
+
 const app = express();
 
 var corsOptions = {
@@ -26,3 +29,25 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on port ${PORT}.`);
 });
+
+db.sequelize.sync({force: true}).then(() => {
+  console.log('Drop and Resync Db');
+  initial();
+});
+
+function initial() {
+  Role.create({
+    id: 1,
+    name: "user"
+  });
+
+  Role.create({
+    id: 2,
+    name: "moderator"
+  });
+
+  Role.create({
+    id: 3,
+    name: "admin"
+  });
+}
