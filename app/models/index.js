693f04f89115bb68339d14ecad9b4edd66de6362
Luis Morales Bernal <luismcodes@gmail.com>
2022-11-23 20:05:48 -0600

    Create Refresh Token Model

--- app/models/index.js
+++ app/models/index.js
@@ -26,6 +26,7 @@ db.sequelize = sequelize;
 
 db.user = require("../models/user.model.js")(sequelize, Sequelize);
 db.role = require("../models/role.model.js")(sequelize, Sequelize);
+db.refreshToken = require("../models/refreshToken.model.js")(sequelize, Sequelize); // v1.1.0
 
 db.role.belongsToMany(db.user, {
   through: "user_roles",
@@ -36,6 +37,12 @@ db.user.belongsToMany(db.role, {
   through: "user_roles",
   foreignKey: "userId",
   otherKey: "roleId"
+});
+db.refreshToken.belongsTo(db.user, {
+  foreignKey: 'userId', targetKey: 'id'
+});
+db.user.hasOne(db.refreshToken, {
+  foreignKey: 'userId', targetKey: 'id'
 });
 
 db.ROLES = ["user", "admin", "moderator"];
